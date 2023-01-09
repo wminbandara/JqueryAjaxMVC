7dd4861e1a35c6338b2aff0eda0ec200ac6180af
Indika <easysolutionsrilanka@gmail.com>
2023-01-09 17:06:37 +0530

    Initial Project Adding

---
+++ JqueryAjaxMVC/Controllers/EmployeeController.cs
@@ -0,0 +1,48 @@
+﻿using JqueryAjaxMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JqueryAjaxMVC.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        //
+        // GET: /Employee/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //Implement action to view all
+        public ActionResult ViewAll()
+        {
+            return View(GetAllEmployee());
+        }
+
+        IEnumerable<Employee>GetAllEmployee()
+        {
+            using (DBModel db = new DBModel())
+            {
+                return db.Employees.ToList<Employee>();
+            }
+        }
+
+        //Implement action for Inserting
+
+        public ActionResult AddOrEdit(int id = 0)
+        {
+            Employee emp = new Employee();
+
+            return View(emp);
+        }
+
+        [HttpPost]
+        public ActionResult AddOrEdit()
+        {
+            return View();
+        }
+	}
+}
---
+++ JqueryAjaxMVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="JqueryAjaxMVC.MvcApplication" Language="C#" %>
